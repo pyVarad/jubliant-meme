5a01d588dc0db3564d86c6588960f0bf7e8766b4
Varad <py.varad@gmail.com>
2017-03-19 06:24:43 +0530

    Updated setting up of mongo and open the port forwarding for mongo

--- jubliant-meme/puppet/modules/cabriolet/manifests/commonrhel.pp
+++ jubliant-meme/puppet/modules/cabriolet/manifests/commonrhel.pp
@@ -1,16 +1,18 @@
+## Common configuration for yum package managers.
 class cabriolet::commonrhel {
-  class { "cabriolet::commonrhel::dependencies": } ->
-  class { "cabriolet::commonrhel::yumRepo": }
+  class { 'cabriolet::commonrhel::dependencies': } ->
+  class { 'cabriolet::commonrhel::yumRepo': }
 }
 
+## Dependency setup here.
 class cabriolet::commonrhel::dependencies {
   $deb_pkg = [
-              "coreutils",
-              "curl",
-              "git",
-              "htop",
-              "ntp",
-              "tzdata",
+              'coreutils',
+              'curl',
+              'git',
+              'htop',
+              'ntp',
+              'tzdata',
               ]
 
   package { 'iptables-services':
@@ -19,17 +21,18 @@ class cabriolet::commonrhel::dependencies {
 
   firewall { '100 Allow http and https access':
     dport   => [80, 443],
-    proto  => tcp,
-    action => accept,
+    proto   => tcp,
+    action  => accept,
     require => Package['iptables-services']
   }
 }
 
+## EPEL Repo configuration ##
 class cabriolet::commonrhel::yumRepo {
   file {'GPGKEY':
-    ensure  => 'present',
-    source  => 'puppet:///modules/cabriolet/epel/RPM-GPG-KEY-EPEL-7',
-    path    => '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7',
+    ensure => 'present',
+    source => 'puppet:///modules/cabriolet/epel/RPM-GPG-KEY-EPEL-7',
+    path   => '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7',
   }
 
   yumrepo { 'epelRelease':
--- jubliant-meme/puppet/modules/cabriolet/manifests/mongo.pp
+++ jubliant-meme/puppet/modules/cabriolet/manifests/mongo.pp
@@ -1,23 +1,34 @@
+# Setting up mongo #
 class cabriolet::mongo {
-  class { "cabriolet::mongo::setup": } ->
-  class { "cabriolet::mongo::startService": }
+    class { 'cabriolet::mongo::setup': } ->
+    class { 'cabriolet::mongo::startService': }
 }
 
+# Add the repo and install the package mongo
 class cabriolet::mongo::setup {
-  file {'MongoRepoKeys':
-    path => '/etc/yum.repos.d/mongodb-org-3.4.repo',
-    source => 'puppet:///modules/cabriolet/mongo/mongodb-org-3.4.repo'
-  }
+    file {'MongoRepoKeys':
+        path   => '/etc/yum.repos.d/mongodb-org-3.4.repo',
+        source => 'puppet:///modules/cabriolet/mongo/mongodb-org-3.4.repo'
+    }
 
-  package { 'mongodb-org':
-    ensure => '3.4.2-1.el7',
-    require => File['MongoRepoKeys']
-  }
+    package { 'mongodb-org':
+        ensure  => '3.4.2-1.el7',
+        require => File['MongoRepoKeys']
+    }
 }
 
+# Start the service
 class cabriolet::mongo::startService {
-  service { 'mongod':
-    ensure => running,
-    enable => true
-  }
+    service { 'mongod':
+        ensure => running,
+        enable => true
+    }
+
+    file { 'mongodb-conf':
+        ensure => file,
+        mode   => '0644',
+        path   => '/etc/mongod.conf',
+        source => 'puppet:///modules/cabriolet/mongo/mongodb.conf',
+        notify => Service['mongod']
+    }
 }
